2d4b5fb98529d5a747d6f6deea038b46e3cc6fc2
Ronita Chhim <ronitachhim@mbp-de-ronita.lan>
2022-08-03 15:10:22 +0200

    server heroku doesn't work

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // import packages
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const cors = require("cors");
 
-mongoose.connect("mongodb://localhost/backend-marvel");
+// mongoose.connect("mongodb://localhost/backend-marvel");
+// mongoose.connect(process.env.DATABASE_URL);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.json("welcome on Marvel Api");
 
+  app.all("*", (req, res) => {
+    res.status(400).json("Route introuvable");
+  });
+});
 app.listen(process.env.PORT, () => {
   console.log("server has started");
 });
