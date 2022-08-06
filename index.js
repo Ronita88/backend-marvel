835486de6a0e7418ef366368869c4ab91804c6c6
Ronita Chhim <ronitachhim@macbook-pro-de-ronita.home>
2022-08-06 21:39:13 +0200

    test

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ app.get("/characters", async (req, res) => {
 app.get("/comics", async (req, res) => {
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${req.query.limit}&skip=${req.query.page}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${
+        process.env.MARVEL_API_KEY
+      }&limit=${req.query.limit}&skip=${req.query.page * req.query.limit}`
     );
     res.json(response.data);
   } catch (error) {
